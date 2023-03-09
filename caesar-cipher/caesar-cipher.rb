8c05f6f1f68e054adf0e747f92a118b031283b60
Saleamlack <saleamlack7@gmail.com>
2023-03-09 19:20:13 +0300

    Add modulo operator

--- caesar-cipher/caesar-cipher.rb
+++ caesar-cipher/caesar-cipher.rb
@@ -9,7 +9,7 @@ def caesar_cipher(source_string, shift_factor)
             up_case_chr[char_index]
         elsif (low_case_chr.include?(character))
             char_index = low_case_chr.index(character) + shift_factor
-            char_index = char_index low_case_chr.length
+            char_index = char_index % low_case_chr.length
             low_case_chr[char_index]
         else
             character
@@ -17,3 +17,5 @@ def caesar_cipher(source_string, shift_factor)
     end
     new_string.join("")
 end
+
+puts caesar_cipher("Hello, World!", -55)
